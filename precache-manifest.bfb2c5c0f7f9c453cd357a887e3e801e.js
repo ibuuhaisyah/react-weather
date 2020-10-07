b6a01d33a5bad856818d2fd27df5f4cdc9b27a25
rohana <hana@scs.my>
2020-10-07 13:17:07 +0800

    Updates

---
+++ precache-manifest.bfb2c5c0f7f9c453cd357a887e3e801e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad0a1defc114ef06f0536f4af58b160",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "8499be83e71d8620471e",
+    "url": "/react-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a760a2e7654ed47146f0",
+    "url": "/react-weather/static/js/2.c79a88c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.c79a88c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8499be83e71d8620471e",
+    "url": "/react-weather/static/js/main.57339c2d.chunk.js"
+  },
+  {
+    "revision": "0ad93a7dfa216740c589",
+    "url": "/react-weather/static/js/runtime-main.772e4ab5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
